01060e56b53f83a2679aad77e70fdddf0b5f552e
Kamran BigdelyShamloo <kamran@zerokey.com>
2016-08-10 14:43:40 -0600

    now showing a cuboid without lighting

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -4,18 +4,44 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	private BitmapFont font;
-
+	public ModelBatch modelBatch;
+	public PerspectiveCamera camera;
+	public Model model;
+	public ModelInstance instance;
 	
 	@Override
 	public void create () {
+		modelBatch = new ModelBatch();
+		camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		camera.position.set(10f, 10f, 10f);
+		camera.lookAt(0,0,0);
+		camera.near = 1f;
+		camera.far = 300f;
+
+		camera.update();
+
+		ModelBuilder modelBuilder = new ModelBuilder();
+		model = modelBuilder.createBox(5.0f, 5.0f, 5.0f,
+				new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+				VertexAttributes.Usage.Position| VertexAttributes.Usage.Normal);
+		instance = new ModelInstance(model);
+
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		font = new BitmapFont();
@@ -24,21 +50,29 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 0);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//		Gdx.gl.glClearColor(0, 0, 0, 0);
+//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
-		batch.begin();
+		modelBatch.begin(camera);
+		modelBatch.render(instance);
+		modelBatch.end();
 
-		//batch.draw(img, 0, 0);
-		font.getData().setScale(6.0f);
-		font.draw(batch, "Hello World from libgdx running in a fragment! :)", 100, 300);
-
-		batch.end();
+//		batch.begin();
+//
+//		//batch.draw(img, 0, 0);
+//		font.getData().setScale(6.0f);
+//		font.draw(batch, "Hello World from libgdx running in a fragment! :)", 100, 300);
+//
+//		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		modelBatch.dispose();
+		model.dispose();
 	}
 }
