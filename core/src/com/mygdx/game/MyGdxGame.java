04fb78508ad2d029fb1a2a757e16047c6435d2ce
Kamran BigdelyShamloo <kamran@zerokey.com>
2016-08-10 17:59:33 -0600

    added gesture detections like zoom, rotating around X and Y (but not Z) axes.

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -14,23 +14,36 @@ import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.Renderable;
+import com.badlogic.gdx.graphics.g3d.Shader;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.DefaultShaderProvider;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.input.GestureDetector;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	//Texture img;
 	private BitmapFont font;
 	public ModelBatch modelBatch;
 	public PerspectiveCamera camera;
 	public Model model;
 	public ModelInstance instance;
 	private Environment environment;
+	private MyCameraInputController cameraController;
 
 	@Override
 	public void create () {
-		modelBatch = new ModelBatch();
+		//modelBatch = new ModelBatch();
+		modelBatch = new ModelBatch(new DefaultShaderProvider() {
+			@Override
+			protected Shader createShader(Renderable renderable) {
+				return new WireframeShader(renderable, config);
+			}
+		});
+
 		camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		camera.position.set(10f, 10f, 10f);
 		camera.lookAt(0,0,0);
@@ -40,6 +53,10 @@ public class MyGdxGame extends ApplicationAdapter {
 		camera.update();
 
 		ModelBuilder modelBuilder = new ModelBuilder();
+//		model = modelBuilder.createBox(5.0f, 5.0f, 5.0f,
+//				new Material(),
+//				VertexAttributes.Usage.Position| VertexAttributes.Usage.Normal);
+
 		model = modelBuilder.createBox(5.0f, 5.0f, 5.0f,
 				new Material(ColorAttribute.createDiffuse(Color.GREEN)),
 				VertexAttributes.Usage.Position| VertexAttributes.Usage.Normal);
@@ -50,21 +67,31 @@ public class MyGdxGame extends ApplicationAdapter {
 		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
 		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
 
+		// Add camera controller
+		//cameraController = new CameraInputController(camera);
+		cameraController = new MyCameraInputController(camera);
+		Gdx.input.setInputProcessor(cameraController);
+		//Gdx.input.setInputProcessor(new GestureDetector(new MyGestureListener()));
+
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		//img = new Texture("badlogic.jpg");
 		font = new BitmapFont();
 		font.setColor(Color.BLUE);
 	}
 
 	@Override
 	public void render () {
+
+		cameraController.update();
+
+
 //		Gdx.gl.glClearColor(0, 0, 0, 0);
 //		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
 		modelBatch.begin(camera);
-		modelBatch.render(instance,environment);
+		modelBatch.render(instance);//,environment);
 		modelBatch.end();
 
 		batch.begin();
@@ -79,7 +106,7 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		//img.dispose();
 		modelBatch.dispose();
 		model.dispose();
 	}
